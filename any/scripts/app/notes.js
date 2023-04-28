a7c9ed6e72885fe18f8af05e2f15148bd8ac8956
pmpwsk <florianpompowski@gmail.com>
2023-04-28 15:15:17 +0200

    moved some stuff from notes.js to note.js, allowing null as note content now

--- any/scripts/app/notes.js
+++ any/scripts/app/notes.js
@@ -32,54 +32,6 @@ async function CreateNote() {
     }
 }
 
-function TextChanged() {
-    let save = document.querySelector("#save");
-    save.innerText = "Save";
-    save.className = "green";
-}
-
-async function Save() {
-    let back = document.querySelector("#back");
-    back.innerText = "Back";
-    let save = document.querySelector("#save");
-    save.innerText = "Saving...";
-    save.className = "green";
-    let textElement = document.querySelector("#text");
-    let text = textElement.value;
-    if (text === "") {
-        text = "null";
-    }
-    try {
-        let response = await fetch("/api/app/notes/save?id=" + id + "&text=" + encodeURIComponent(text));
-        if (response.status === 200) {
-            let text = await response.text();
-            if (text.startsWith("/app/notes")) {
-                save.innerText = "Saved!";
-                save.className = "";
-            } else {
-                save.innerText = "Error!";
-                save.className = "red";
-            }
-        } else {
-            save.innerText = "Error!";
-            save.className = "red";
-        }
-    } catch {
-        save.innerText = "Error!";
-        save.className = "red";
-    }
-}
-
-async function Back(parentLink) {
-    let back = document.querySelector("#back");
-    let save = document.querySelector("#save");
-    if (save.innerText === "Save" && back.innerText == "Back") {
-        back.innerText = "Discard?";
-    } else {
-        window.location.assign(parentLink);
-    }
-}
-
 async function Delete() {
     let deleteElement = document.querySelector("#delete");
     if (deleteElement.textContent === "Really?") {
--- any/scripts/app/notes/note.js
+++ any/scripts/app/notes/note.js
@@ -1,7 +1,9 @@
 let ch = 0;
 let ta = document.querySelector('#text');
 let editor = document.querySelector('#editor');
-let page = document.body.children[1];
+let save = document.querySelector('#save');
+let back = document.querySelector('#back');
+let page = document.querySelector('.page');
 window.onresize = Resize;
 ta.onclick = Refocus;
 Resize();
@@ -42,5 +44,43 @@ async function Load() {
             let save = document.querySelector("#save");
             save.innerText = "Error!";
             save.className = "red";
+    }
+}
+
+function TextChanged() {
+    save.innerText = "Save";
+    save.className = "green";
+}
+
+async function Save() {
+    back.innerText = "Back";
+    save.innerText = "Saving...";
+    save.className = "green";
+    try {
+        let response = await fetch("/api/app/notes/save?id=" + id + "&text=" + encodeURIComponent(ta.value));
+        if (response.status === 200) {
+            let text = await response.text();
+            if (text.startsWith("/app/notes")) {
+                save.innerText = "Saved!";
+                save.className = "";
+            } else {
+                save.innerText = "Error!";
+                save.className = "red";
+            }
+        } else {
+            save.innerText = "Error!";
+            save.className = "red";
+        }
+    } catch {
+        save.innerText = "Error!";
+        save.className = "red";
+    }
+}
+
+async function Back(parentLink) {
+    if (save.innerText === "Save" && back.innerText == "Back") {
+        back.innerText = "Discard?";
+    } else {
+        window.location.assign(parentLink);
     }
 }
