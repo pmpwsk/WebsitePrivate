d445ecf8bccec2714798432425d1a1d1c2e08f0c
pmpwsk <florianpompowski@gmail.com>
2023-05-04 18:51:33 +0200

    new update function

--- any/scripts/server/update.js
+++ any/scripts/server/update.js
@@ -1,3 +1,12 @@
+async function UpdateOld() {
+    let file = document.getElementById('update-file').files[0];
+    let form = new FormData();
+    form.append('file', file);
+    let response = await fetch('/server/update', { method: 'POST', body: form });
+    if (response.status === 200) window.location.assign("/server");
+    else alert("Error!");
+}
+
 async function Update() {
     let file = document.getElementById('update-file').files[0];
     let form = new FormData();
@@ -5,4 +14,20 @@ async function Update() {
     let response = await fetch('/server/update', {method: 'POST', body: form});
     if (response.status === 200) window.location.assign("/server");
     else alert("Error!");
+
+    let request = new XMLHttpRequest();
+    request.open('POST', '/server/update');
+    request.upload.addEventListener('progress', event => {
+        console.log('Uploading... ' + '(' + ((event.loaded / event.total) * 100).toFixed(2) + '%)');
+    });
+    request.onreadystatechange = () => {
+        if (request.readyState == 4) {
+            if (request.status === 200) {
+                window.location.assign("/server");
+            } else {
+                alert("Error!");
+            }
+        }
+    };
+    request.send(uploadFormDate);
 }
