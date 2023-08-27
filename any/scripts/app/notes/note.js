3c6994f76f9ca1b30130c96e4c355a2418151900
Flo <flo@uwap.org>
2023-08-27 20:02:59 +0200

    I don't even remember what I did at this point

--- any/scripts/app/notes/note.js
+++ any/scripts/app/notes/note.js
@@ -1,6 +1,7 @@
 let ch = 0;
 let ta = document.querySelector('#text');
 let editor = document.querySelector('#editor');
+let sidebar = document.querySelector('.sidebar');
 let save = document.querySelector('#save');
 let back = document.querySelector('#back');
 let page = document.querySelector('.content');
@@ -13,7 +14,9 @@ function Resize() {
     let pageComp = window.getComputedStyle(page);
     let editorComp = window.getComputedStyle(editor);
     let newHeight = Math.floor(window.visualViewport.height - parseFloat(editorComp['marginTop']) - parseFloat(pageComp['paddingTop']) - parseFloat(pageComp['paddingBottom']));
-    editor.style.height = newHeight+'px';
+    editor.style.flex = '1';
+    editor.style.height = newHeight + 'px';
+    sidebar.style.height = newHeight + 6.4 + 'px';
     Refocus();
 }
 
