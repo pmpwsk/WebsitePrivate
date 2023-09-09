0f3bdd8add8ef10ee514186f468d5d7b004a79f4
Flo <flo@uwap.org>
2023-09-09 14:18:53 +0200

    removed error.js and redirect.js, updated news

--- any/scripts/error.js
+++ any/scripts/error.js
@@ -1,10 +1,1 @@
-let error = document.querySelector("#error");
-
-function ShowError(message) {
-    error.firstElementChild.innerText = message;
-    error.style.display = "block";
-}
-
-function HideError() {
-    error.style.display = "none";
-}
+alert("Wrong error.js!");
